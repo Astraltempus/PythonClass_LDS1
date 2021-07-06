98692a5f5a8b518a5c509fd3843669fc806c0f22
Astraltempus <astraltempus@gmail.com>
2021-07-06 10:48:24 -0700

    Update bus.py

--- Day_3/bus.py
+++ Day_3/bus.py
@@ -1,5 +1,3 @@
-#"pip install questionary" before use
-
 import questionary
 btype = ''
 
@@ -27,27 +25,41 @@ def calculate():
     if btype == '4 seats':
         idleprice = (wtime - 5) * 750
         if km <= 0.8:
-            distanceprice = km * 11000
-        if km <= 30:
-            distanceprice = km * 15300
-        if km >= 31:
-            distanceprice = km * 12100
+            # # code của bạn
+            # distanceprice = km * 11000 # ở đây là đồng/0.8km
+
+            # đổi ở đây
+            distanceprice = km * 11000 * 10/8 # đây là đã đổi qua đồng/km
+        elif km <= 31: # là trường hợp 2 bao gồm  0.8 km đầu là 0.8km với tiền là 11000 * 10/8 và (km - 0.8)km sau với tiền là 15300 đồng/km
+            distanceprice = (0.8 * 11000 * 10/8) + (km-0.8) * 15300
+        else: # trường hợp cuối 0.8 km với 11000 * 10/8 và (31-0.8) km sau và (km-31) km cuối
+            distanceprice = 0.8 * 11000 * 10/8 + (31-0.8) * 15300 + (km-31) * 12100
         total = distanceprice + idleprice
         print("IdlePrice: ", idleprice)
         print("DistancePrice: ", distanceprice)
         print("Total: ", total)
+
+        # # ra dạng 100,000,000
+        # print(f"IdlePrice:  {idleprice:,}")
+        # print(f"DistancePrice: {distanceprice:,}")
+        # print(f"Total: {total:,}")
     if btype == '7 seats':
+        # tương tự
         idleprice = (wtime - 5) * 750
         if km <= 0.8:
-            distanceprice = km * 12000
-        if km <= 30:
-            distanceprice = km * 16100
-        if km >= 31:
-            distanceprice = km * 13800
+            distanceprice = km * 12000 * 10/8
+        elif km <= 31:
+            distanceprice = (0.8 * 12000 * 10/8) + (km-0.8) * 16100
+        else:
+            distanceprice = (0.8 * 12000 * 10/8) + (31-0.8) * 16100 + (km-31) * 13800
         total = distanceprice + idleprice
         print("IdlePrice: ", idleprice)
         print("DistancePrice: ", distanceprice)
         print("Total: ", total)
 
-calculate()
+        # # ra dạng 100,000,000
+        # print(f"IdlePrice:  {idleprice:,}")
+        # print(f"DistancePrice: {distanceprice:,}")
+        # print(f"Total: {total:,}")
 
+calculate()
