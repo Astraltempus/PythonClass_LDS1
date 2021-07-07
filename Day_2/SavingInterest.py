b2fcd524f1bec22122632d6bd9d5f3b8e23d6639
Astraltempus <77897800+Astraltempus@users.noreply.github.com>
2021-07-07 16:20:49 -0700

    Delete Day_2 directory

--- Day_2/SavingInterest.py
+++
@@ -1,8 +0,0 @@
-IntPercent = float(input("Interest Rate: (%) "))
-Balance = int(input("Balance: "))
-Months = int(input("How many months? "))
-
-IntMoney = (Balance*Months)*(IntPercent/100/12)
-Total = Balance + IntMoney
-print("******************RESULT******************")
-print("Total amount: ", Total)
