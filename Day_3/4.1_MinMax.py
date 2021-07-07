fda15efa5e89a2c9803e954906654be608828c77
Astraltempus <77897800+Astraltempus@users.noreply.github.com>
2021-07-07 16:20:43 -0700

    Delete Day_3 directory

--- Day_3/4.1_MinMax.py
+++
@@ -1,8 +0,0 @@
-nums = []
-
-num = int(input('How many numbers: '))
-
-for inputs in range(num):
-    numbers = int(input('Enter numbers:'))
-    nums.append(numbers)
-print("Max is:", max(nums), "\nMin is :", min(nums))
